134c37385c4cef4cec48b991bc25e3becf9425b1
anand m <anand@resistance.net>
2017-08-13 14:17:25 -0700

    added documentation and arg to choose method

--- vehicle_detector.py
+++ vehicle_detector.py
@@ -1,6 +1,21 @@
 """
 Vehicle Detector
-   Will detect and add visualizations around vehicles to static image or a video
+    Will detect and add visualizations around vehicles to static image or a video
+
+    To setup YAD2K, follow these directions...
+        1) git clone https://github.com/allanzelener/yad2k.git
+        2) cd yad2k
+        3) wget https://github.com/pjreddie/darknet/raw/master/cfg/tiny-yolo-voc.cfg
+        4) wget https://pjreddie.com/media/files/tiny-yolo-voc.weights
+        5) python yad2k.py tiny-yolo-voc.cfg tiny-yolo-voc.weights model_data/tiny-yolo-voc.h5
+
+    To setup darknet, follow these directions...
+        1) git clone https://github.com/pjreddie/darknet
+        2) cd darknet
+        3) edit Makefile if using GPU, CUDNN, etc.
+        4) make
+        5) wget https://pjreddie.com/media/files/tiny-yolo-voc.weights
+        6) edit cfg/voc.data to add "darknet" to "data/voc.names"
 """
 
 import os
@@ -15,7 +30,7 @@ import h5py
 from keras import backend as K
 from keras.models import load_model
 from keras import __version__ as keras_version
-from yad2k.models.keras_yolo import yolo_eval, yolo_head
+from yad2k.yad2k.models.keras_yolo import yolo_eval, yolo_head
 import darknet
 import ctypes
 
@@ -235,12 +250,14 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
     parser.add_argument('--cal', type=str, metavar="FILENAME", default="camera_cal.npz",
                         help='filename with camera calibration matrix & distortion coefficients (DEFAULT: camera_cal.npz)')
-    parser.add_argument('--model', type=str, metavar="FILENAME", default="yolo.h5",
-                        help='filename with YOLO model and weights (DEFAULT: yolo.h5)')
-    parser.add_argument('--anchors', type=str, metavar="FILENAME", default="yolo_anchors.txt",
-                        help='filename with YOLO anchors (DEFAULT: yolo_anchors.txt)')
-    parser.add_argument('--classes', type=str, metavar="FILENAME", default="yolo_classes.txt",
-                        help='filename with YOLO anchors (DEFAULT: yolo_classes.txt)')
+    parser.add_argument('--method', type=str, choices=['yad2k', 'darknet'], default='darknet',
+                        help='which detection method to use (DEFAULT: darknet')
+    parser.add_argument('--model', type=str, metavar="FILENAME", default="yad2k/model_data/tiny-yolo-voc.h5",
+                        help='filename with YOLO model and weights (DEFAULT: yad2k/model_data/tiny-yolo-voc.h5)')
+    parser.add_argument('--anchors', type=str, metavar="FILENAME", default="yad2k/model_data/tiny-yolo_anchors.txt",
+                        help='filename with YOLO anchors (DEFAULT: yad2k/model_data/tiny-yolo_anchors.txt)')
+    parser.add_argument('--classes', type=str, metavar="FILENAME", default="yad2k/model_data/pascal_classes.txt",
+                        help='filename with YOLO anchors (DEFAULT: yad2k/model_data/pascal_classes.txt)')
     parser.add_argument('--display', action="store_true", help='display augmented images/video')
     parser.add_argument('--debug', action="store_true", help=argparse.SUPPRESS)
     parser.add_argument('--outdir', type=str, metavar="OUTDIR", default=".",
@@ -255,38 +272,43 @@ if __name__ == "__main__":
     cameraMatrix, distCoeffs, newCameraMatrix, validPixROI = load_camera_calibration(args.cal)
 
     # check that model Keras version is same as local Keras version
-    """
-    f = h5py.File(args.model, mode='r')
-    model_version = f.attrs.get('keras_version')
-    keras_version = str(keras_version).encode('utf8')
+    if args.method == 'yad2k':
+        f = h5py.File(args.model, mode='r')
+        model_version = f.attrs.get('keras_version')
+        keras_version = str(keras_version).encode('utf8')
 
-    if model_version != keras_version:
-        print('You are using Keras version ', keras_version,
-              ', but the model was built using ', model_version)
+        if model_version != keras_version:
+            print('You are using Keras version ', keras_version,
+                  ', but the model was built using ', model_version)
 
-    # load Keras model with weights
-    model = load_model(args.model)
+        # load Keras model with weights
+        model = load_model(args.model)
 
-    # read classes file
-    with open(args.classes) as f:
-        classes = f.readlines()
-    classes = [c.strip() for c in classes]
+        # read classes file
+        with open(args.classes) as f:
+            classes = f.readlines()
+        classes = [c.strip() for c in classes]
 
-    # read anchors file
-    with open(args.anchors) as f:
-        anchors = f.readline()
-        anchors = [float(x) for x in anchors.split(',')]
-        anchors = np.array(anchors).reshape(-1, 2)
+        # read anchors file
+        with open(args.anchors) as f:
+            anchors = f.readline()
+            anchors = [float(x) for x in anchors.split(',')]
+            anchors = np.array(anchors).reshape(-1, 2)
 
-    # verify model, anchors, and classes are compatible
-    num_classes = len(classes)
-    num_anchors = len(anchors)
-    model_output_channels = model.layers[-1].output_shape[-1]
-    assert model_output_channels == num_anchors * (num_classes + 5), \
-        'Mismatch between model and given anchor and class sizes. ' \
-        'Specify matching anchors and classes with --anchors and ' \
-        '--classes flags.'
-    """
+        # verify model, anchors, and classes are compatible
+        num_classes = len(classes)
+        num_anchors = len(anchors)
+        model_output_channels = model.layers[-1].output_shape[-1]
+        assert model_output_channels == num_anchors * (num_classes + 5), \
+            'Mismatch between model and given anchor and class sizes. ' \
+            'Specify matching anchors and classes with --anchors and ' \
+            '--classes flags.'
+
+        # open TF session
+        sess = K.get_session()  # TODO: Remove dependence on Tensorflow session.
+    elif args.method == 'darknet':
+        net = darknet.load_net(b"darknet/cfg/tiny-yolo-voc.cfg", b"darknet/tiny-yolo-voc.weights", 0)
+        meta = darknet.load_meta(b"darknet/cfg/voc.data")
 
     # create output directory if needed
     if not os.path.exists(args.outdir):
@@ -294,12 +316,6 @@ if __name__ == "__main__":
 
     # start FPS timer
     fps = FPS().start()
-
-    # open TF session
-    sess = K.get_session()  # TODO: Remove dependence on Tensorflow session.
-
-    net = darknet.load_net(b"darknet/cfg/tiny-yolo-voc.cfg", b"darknet/tiny-yolo-voc.weights", 0)
-    meta = darknet.load_meta(b"darknet/cfg/voc.data")
 
     for infile in args.infiles:
         print("Processing {0}...".format(infile))
@@ -313,14 +329,14 @@ if __name__ == "__main__":
             video = True
 
         # go through each frame in image (only 1 frame, obviously) or video
-        """
-        outclip = clip.fl_image(lambda frame: detect_vehicles_yad2k(frame, fps, cameraMatrix, distCoeffs,
-                                                              sess, model, classes, anchors,
-                                                              isVideo=video, display=args.display, debug=args.debug))
-        """
-        outclip = clip.fl_image(lambda frame: detect_vehicles_darknet(frame, fps, cameraMatrix, distCoeffs,
-                                                                      net, meta,
-                                                                      isVideo=video, display=args.display, debug=args.debug))
+        if args.method == 'yad2k':
+            outclip = clip.fl_image(lambda frame: detect_vehicles_yad2k(frame, fps, cameraMatrix, distCoeffs,
+                                                                  sess, model, classes, anchors,
+                                                                  isVideo=video, display=args.display, debug=args.debug))
+        elif args.method == 'darknet':
+            outclip = clip.fl_image(lambda frame: detect_vehicles_darknet(frame, fps, cameraMatrix, distCoeffs,
+                                                                          net, meta,
+                                                                          isVideo=video, display=args.display, debug=args.debug))
 
         outfile = args.outdir + "/" + os.path.basename(infile)
         if video:
@@ -338,6 +354,7 @@ if __name__ == "__main__":
     print("[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
     print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
 
-    # close TF session
-    sess.close()
+    if args.method == 'yad2k':
+        # close TF session
+        sess.close()
 
